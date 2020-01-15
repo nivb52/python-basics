0179cbbbe2c8cac043126562dd804b21c2cf40ca
Niv B <nivb52@gmail.com>
2020-01-15 16:26:39 +0200

    data structures - list

---
+++ 03- Data Structures/01-Lists.py
@@ -0,0 +1,20 @@
+
+letters = ['a', 'b', 'c']
+number = [1, 2, 3]
+matrix = [[0, 1], [2, 3]]
+# // concat in python
+combinedList = number + letters
+
+# // CREATE LONG LIST QUICKLY : 
+# 1:
+zeros = [0] * 100
+# 2:
+numbers = list (range(20))
+print(numbers)
+
+# // USE STRINGS
+chars = list("Hello world")
+print (chars)
+
+# // .LENTH == len()
+print (len(chars))
