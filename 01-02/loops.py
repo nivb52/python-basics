4e609df3378f4360f31de9df08450597360cb99e
Niv B <nivb52@gmail.com>
2020-01-15 16:28:18 +0200

    loops

---
+++ 01-02/loops.py
@@ -0,0 +1,15 @@
+    # // Ctrl +  Alt + n for code runner
+# guess = 0
+# answer = 5
+# while answer != guess:
+#     guess = int(input("guees a number:"))
+
+# else: # else for the while loop
+#     pass
+
+
+for l in 'Python': 
+    print(l)
+
+
+        print 
