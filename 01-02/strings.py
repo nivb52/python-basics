8fce63960fe40f3aaf4359d06e8f418a28a185da
Niv B <nivb52@gmail.com>
2020-01-15 16:30:20 +0200

    strings

---
+++ 01-02/strings.py
@@ -0,0 +1,19 @@
+count = 1000
+price = 3.44
+type(count)
+
+# Back Tik
+myTitle= "Primnister Said: \" I will not go to another allection"
+firstSection = """ This is a uniqe type 
+you could write and enter rows and it acts as 
+an HTML <pre> """
+
+# 
+first = "Niv"
+last = "B"
+fullName = f"{first} {last}"
+print(fullName)
+
+
+# len(str) in python ==>  str.length in JS
+
